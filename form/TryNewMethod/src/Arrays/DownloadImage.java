e7568b848d261d4258ba0a8f064022f63ebc3c26
Adithya Bandara <dammikaekanayaka1980@gmail.com>
2023-04-13 01:50:20 -0700

    namechanges

--- form/TryNewMethod/src/Arrays/DownloadImage.java
+++ form/TryNewMethod/src/Arrays/DownloadImage.java
@@ -1,4 +1,4 @@
-package trynewmethod;
+package Arrays;
 
 import java.io.File;
 import java.io.FileOutputStream;
--- form/TryNewMethod/src/Arrays/LinearSearch.java
+++ form/TryNewMethod/src/Arrays/LinearSearch.java
@@ -2,11 +2,11 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
-package trynewmethod;
+package Arrays;
 
 import java.util.Scanner;
 
-public class TryNewMethod {
+public class LinearSearch {
 
     public static int linearSearch(int[] arr, int target) {
         for(int i = 0; i < arr.length; i++) {
